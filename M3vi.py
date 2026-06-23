4c57af9eac74cb9c4c3e0cbb7bba163ce45f0c44
UDoorman1717 <maviipeklioglu54@gmail.com>
2026-06-23 14:33:08 +0300

    Update M3vi.py

--- M3vi.py
+++ M3vi.py
@@ -1,5 +1,8 @@
 import discord
 from discord.ext import commands
+import os
+import random
+import requests
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -42,5 +45,18 @@ async def cool(ctx):
     """
     if ctx.invoked_subcommand is None:
         await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+        
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''duck komutunu çağırdığımızda, program ordek_resmi_urlsi_al fonksiyonunu çağırır.'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
 
 bot.run("")
